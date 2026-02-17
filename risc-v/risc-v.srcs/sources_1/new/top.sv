528b3c993b628dccb5d174e011fec9864be9ba75
Rohan Rao <rohankr2@illinois.edu>
2026-02-17 03:59:14 -0600

    fetch init

--- risc-v/risc-v.srcs/sources_1/new/top.sv
+++ risc-v/risc-v.srcs/sources_1/new/top.sv
@@ -1,9 +1,17 @@
 `timescale 1ns / 1ps
 
 module top(
-    input clk
+    input clk,
+    input rst_n
 );
-        
+    import rv_pipe_pkg::*;
 
+    pipe_if #(.T(if_id_t)) if_id_bus;
+
+    fetch_stage u_fetch(
+        .clk(clk),
+        .rst_n(rst_n),
+        .if_id_out(if_id_bus)
+    );
 
 endmodule
